4359115024b49ef65581078715e354cc80567f95
123456 <wenmouememail@.com>
2020-09-27 17:20:10 +0800

    commfit:config emali

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //使用AndroidStuodio上传GieHub
-        //fork下来的项目
+        //fork下来的项目改了用户名
     }
 }
